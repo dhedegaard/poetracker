7733d076b772292159240488f38487b4a001dc6d
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-05-19 17:39:02 +0200

    whoops, ignored the webpack config :)

---
+++ Web/webpack.config.js
@@ -0,0 +1,27 @@
+﻿const { CheckerPlugin } = require('awesome-typescript-loader');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'frontend'),
+    entry: {
+        bundle: './index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'wwwroot'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CheckerPlugin()
+    ]
+};
