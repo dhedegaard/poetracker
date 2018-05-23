588671908113fa1f3312b1c6f30cefcd44ea303a
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-05-23 21:01:28 +0200

    core: Added TwitchUsername field to the Account model.

---
+++ Core/Migrations/20180523185854_account__twitchusername.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Migrations
+{
+    public partial class account__twitchusername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "bravoscript");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "johnconor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwitchUsername",
+                table: "Accounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwitchUsername",
+                table: "Accounts");
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                column: "AccountName",
+                value: "johnconor");
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                column: "AccountName",
+                value: "bravoscript");
+        }
+    }
+}
--- Core/Models/Account.cs
+++ Core/Models/Account.cs
@@ -5,18 +5,15 @@ namespace Core.Models {
     public class Account {
         [Key]
         public string AccountName { get; set; }
+        public string TwitchUsername { get; set; }
 
         public override string ToString() =>
-            $"<{nameof(Account)} AccountName=\"{AccountName}\">";
+            $"<{nameof(Account)} AccountName=\"{AccountName}\" TwitchUsername=\"{TwitchUsername}\">";
 
         public static void OnModelCreating(ModelBuilder modelBuilder) {
             var entity = modelBuilder.Entity<Account>();
 
             entity.HasKey(e => e.AccountName);
-
-            entity.HasData(
-                new Account { AccountName = "johnconor" },
-                new Account { AccountName = "bravoscript" });
         }
     }
 }
