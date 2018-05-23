83a208286b93d2177da6d4d5657ef61785c2434e
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-05-23 21:10:05 +0200

    core: Added some better seed data for the Account model.

---
+++ Core/Migrations/20180523190846_account__better_seed_data.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Migrations
+{
+    public partial class account__better_seed_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountName", "TwitchUsername" },
+                values: new object[,]
+                {
+                    { "nugiyen", "nugiyen" },
+                    { "helman", "Helman" },
+                    { "raizqt", "RaizQT" },
+                    { "dclara", "DCLara1" },
+                    { "havoc6", "havoc616" },
+                    { "alkaizerx", "alkaizerx" },
+                    { "pinytenis", "Bakedchicken" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "alkaizerx");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "dclara");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "havoc6");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "helman");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "nugiyen");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "pinytenis");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountName",
+                keyValue: "raizqt");
+        }
+    }
+}
--- Core/Models/Account.cs
+++ Core/Models/Account.cs
@@ -7,6 +7,10 @@ namespace Core.Models {
         public string AccountName { get; set; }
         public string TwitchUsername { get; set; }
 
+        public string TwitchURL {
+            get => !string.IsNullOrEmpty(TwitchUsername) ? $"https://www.twitch.tv/{TwitchUsername}" : null;
+        }
+
         public override string ToString() =>
             $"<{nameof(Account)} AccountName=\"{AccountName}\" TwitchUsername=\"{TwitchUsername}\">";
 
@@ -14,6 +18,17 @@ namespace Core.Models {
             var entity = modelBuilder.Entity<Account>();
 
             entity.HasKey(e => e.AccountName);
+
+            // Some popular streamers :)
+            entity.HasData(
+                new Account { AccountName = "nugiyen", TwitchUsername = "nugiyen" },
+                new Account { AccountName = "helman", TwitchUsername = "Helman" },
+                new Account { AccountName = "raizqt", TwitchUsername = "RaizQT" },
+                new Account { AccountName = "dclara", TwitchUsername = "DCLara1" },
+                new Account { AccountName = "havoc6", TwitchUsername = "havoc616" },
+                new Account { AccountName = "alkaizerx", TwitchUsername = "alkaizerx" },
+                new Account { AccountName = "pinytenis", TwitchUsername = "Bakedchicken" }
+            );
         }
     }
 }
