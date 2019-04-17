19ea7aab91c5ec7e3a188e6da1b19c9197775169
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-04-17 20:24:09 +0200

    Fixed build in dotnet 2.2

--- Core/Apis/CharactersApi.cs
+++ Core/Apis/CharactersApi.cs
@@ -5,23 +5,24 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Core.Apis {
-    public class WindowCharacter {
-        public string Name { get; set; }
-        public string League { get; set; }
-        public int ClassId { get; set; }
-        public int AscendancyClass { get; set; }
-        public string Class { get; set; }
-        public int Level { get; set; }
-        public long Experience { get; set; }
-        public bool? LastActive { get; set; }
+  public class WindowCharacter {
+    public string Name { get; set; }
+    public string League { get; set; }
+    public int ClassId { get; set; }
+    public int AscendancyClass { get; set; }
+    public string Class { get; set; }
+    public int Level { get; set; }
+    public long Experience { get; set; }
+    public bool? LastActive { get; set; }
+  }
+  public static class CharactersApi {
+    public async static Task<IList<WindowCharacter>> GetCharacters(string accountName) {
+      using (var client = new WebClient()) {
+        var data = await client.DownloadStringTaskAsync(new Uri(
+            $"https://www.pathofexile.com/character-window/get-characters?accountName={accountName}"
+        ));
+        return JsonConvert.DeserializeObject<IList<WindowCharacter>>(data);
+      }
     }
-    public static class CharactersApi {
-        public async static Task<IList<WindowCharacter>> GetCharacters(string accountName) {
-            using var client = new WebClient();
-            var data = await client.DownloadStringTaskAsync(new Uri(
-                $"https://www.pathofexile.com/character-window/get-characters?accountName={accountName}"
-            ));
-            return JsonConvert.DeserializeObject<IList<WindowCharacter>>(data);
-        }
-    }
+  }
 }
