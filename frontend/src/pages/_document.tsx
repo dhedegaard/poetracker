e4c0066a820b7c57d0396035a4290666ca8597d2
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-10-31 11:53:23 +0100

    It render everything somewhat corerect.

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -26,5 +26,90 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content="#ffffff" />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+            integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+            asp-append-version="true"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+            asp-append-version="true"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+            asp-append-version="true"
+          />
+          <link
+            rel="manifest"
+            href="/manifest.webmanifest"
+            asp-append-version="true"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <title>Poetracker</title>
+          <meta name="robots" content="noindex,nofollow" />
+          <meta property="og:title" content="Poetracker" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Path of Exile rank tracker"
+          />
+          <meta property="og:image" content="/favicon.png" />
+        </Head>
+        <body className="bg-light">
+          <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+              <span className="navbar-brand">
+                <img
+                  src="/favicon-32x32.png"
+                  width="30"
+                  height="30"
+                  alt="Poetracker"
+                  asp-append-version="true"
+                />
+                Poetracker
+              </span>
+              <a
+                className="navbar-text text-right"
+                href="https://github.com/dhedegaard/poetracker"
+                target="_blank"
+                rel="noopener"
+              >
+                Github
+              </a>
+            </div>
+          </nav>
+          <div className="container">
+            <Main />
+          </div>
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
