1b851c3b3decbd47bb020f1d74aef678f99bec33
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-11-01 22:54:38 +0100

    Converted more stuff to MUI components.

--- frontend/src/components/CharacterTable/Row.tsx
+++ frontend/src/components/CharacterTable/Row.tsx
@@ -1,8 +1,18 @@
+import {
+  Box,
+  Chip,
+  Link,
+  TableCell,
+  TableRow,
+  Typography,
+} from '@material-ui/core'
+import Image from 'next/image'
 import React from 'react'
 import styled from 'styled-components'
-
 import CharacterGraphContainer from '../../containers/character-graph'
+import Badge from '../Badge'
 
+/** @deprecated TODO: Reimplement me? */
 const TwitchBadge = styled.a`
   &,
   &:link,
@@ -23,7 +33,7 @@ interface IProps {
   isSelected: boolean
 }
 
-const Row: React.FunctionComponent<IProps> = props => {
+const Row: React.FunctionComponent<IProps> = (props) => {
   const { datapoint, isSelected } = props
 
   const onRowClick = () => {
@@ -35,129 +45,129 @@ const Row: React.FunctionComponent<IProps> = props => {
 
   return (
     <>
-      <tr
-        className={isSelected ? 'table-active' : undefined}
-        onClick={onRowClick}
-      >
-        <td className="text-nowrap">
-          <img
-            src={`/${datapoint.datapoint.online ? 'online' : 'offline'}.png`}
-            title={datapoint.datapoint.online ? 'Online' : 'Offline'}
-            width={15}
-            height={15}
-          />{' '}
-          <span>{datapoint.datapoint.globalRank || '15000+'}</span>
-          {datapoint.previousDatapoint &&
-            (datapoint.previousDatapoint.globalRank || 15001) !==
-              (datapoint.datapoint.globalRank || 15001) && (
-              <>
-                {' '}
-                <small
-                  title={
-                    datapoint.previousDatapoint &&
-                    datapoint.previousDatapoint.timestamp
-                      ? `Compared to: ${new Date(
-                          datapoint.previousDatapoint.timestamp
-                        ).toLocaleString()}`
-                      : undefined
-                  }
-                  className={
-                    'badge ' +
-                    ((datapoint.datapoint.globalRank || 15001) <
-                    (datapoint.previousDatapoint.globalRank || 15001)
-                      ? 'badge-success'
-                      : 'badge-danger')
-                  }
-                >
-                  {String.fromCharCode(
-                    (datapoint.datapoint.globalRank || 15001) >
+      <TableRow selected={isSelected} onClick={onRowClick}>
+        <TableCell>
+          <Box display="flex" whiteSpace="nowrap">
+            <Image
+              src={`/${datapoint.datapoint.online ? 'online' : 'offline'}.png`}
+              title={datapoint.datapoint.online ? 'Online' : 'Offline'}
+              width="15px"
+              height="15px"
+            />{' '}
+            <Typography>
+              {datapoint.datapoint.globalRank || '15000+'}
+            </Typography>
+            {datapoint.previousDatapoint &&
+              (datapoint.previousDatapoint.globalRank || 15001) !==
+                (datapoint.datapoint.globalRank || 15001) && (
+                <>
+                  {' '}
+                  <Badge
+                    type={
+                      (datapoint.datapoint.globalRank || 15001) <
                       (datapoint.previousDatapoint.globalRank || 15001)
-                      ? 8595
-                      : 8593
-                  )}{' '}
-                  {Math.abs(
-                    (datapoint.datapoint.globalRank || 15001) -
-                      (datapoint.previousDatapoint.globalRank || 15001)
-                  )}
-                </small>
-              </>
-            )}
-        </td>
-        <td
-          className="text-nowrap"
+                        ? 'success'
+                        : 'failure'
+                    }
+                    label={`${String.fromCharCode(
+                      (datapoint.datapoint.globalRank || 15001) >
+                        (datapoint.previousDatapoint.globalRank || 15001)
+                        ? 8595
+                        : 8593
+                    )} ${Math.abs(
+                      (datapoint.datapoint.globalRank || 15001) -
+                        (datapoint.previousDatapoint.globalRank || 15001)
+                    )}`}
+                    title={
+                      datapoint.previousDatapoint &&
+                      datapoint.previousDatapoint.timestamp
+                        ? `Compared to: ${new Date(
+                            datapoint.previousDatapoint.timestamp
+                          ).toLocaleString()}`
+                        : undefined
+                    }
+                  />
+                </>
+              )}
+          </Box>
+        </TableCell>
+        <TableCell
           title={
             `Account name: ${datapoint.datapoint.accountId}\n` +
             `League: ${datapoint.datapoint.leagueId}`
           }
         >
-          <div className="float-right d-none d-sm-block">
-            {datapoint.datapoint.account.twitchURL && (
-              <>
-                <TwitchBadge
-                  href={datapoint.datapoint.account.twitchURL}
-                  target="_blank "
-                  rel="nofollow noreferrer"
-                  className="badge badge-dark"
-                  title={`Twitch username: ${
-                    datapoint.datapoint.account.twitchUsername
-                  }`}
-                >
-                  Twitch
-                </TwitchBadge>{' '}
-              </>
-            )}
-            <a
-              href={datapoint.datapoint.poeProfileURL}
-              target="_blank"
-              rel="nofollow noreferrer"
-              className="badge badge-info"
-            >
-              Profile
-            </a>
-          </div>
-          {datapoint.datapoint.charname}
-          {datapoint.datapoint.dead && (
-            <>
-              {' '}
-              <small className="badge badge-danger">Dead</small>
-            </>
-          )}
-        </td>
-        <td>{datapoint.datapoint.class}</td>
-        <td className="text-right text-nowrap">
+          <Box
+            width="100%"
+            display="flex"
+            justifyContent="space-between"
+            flexGrow="0"
+            whiteSpace="nowrap"
+          >
+            <Typography>
+              {datapoint.datapoint.charname}
+              {datapoint.datapoint.dead && (
+                <>
+                  {' '}
+                  <Chip size="small" label="Dead" color="secondary" />
+                </>
+              )}
+            </Typography>
+            <Box>
+              {datapoint.datapoint.account.twitchURL && (
+                <>
+                  <Badge
+                    type="twitch"
+                    href={datapoint.datapoint.account.twitchURL}
+                    target="_blank "
+                    rel="nofollow noreferrer"
+                    title={`Twitch username: ${datapoint.datapoint.account.twitchUsername}`}
+                    label="Twitch"
+                  />{' '}
+                </>
+              )}
+              <Badge
+                type="profile"
+                href={datapoint.datapoint.poeProfileURL}
+                target="_blank"
+                rel="nofollow noreferrer"
+                label="Profile"
+              />
+            </Box>
+          </Box>
+        </TableCell>
+        <TableCell>{datapoint.datapoint.class}</TableCell>
+        <TableCell align="right">
           {datapoint.previousDatapoint &&
             datapoint.previousDatapoint.level !== datapoint.datapoint.level && (
-              <>
-                <span
-                  title={
-                    datapoint.previousDatapoint &&
-                    datapoint.previousDatapoint.timestamp
-                      ? `Compared to: ${new Date(
-                          datapoint.previousDatapoint.timestamp
-                        ).toLocaleString()}`
-                      : undefined
-                  }
-                  className="badge badge-success"
-                >
-                  +
-                  {datapoint.datapoint.level -
-                    datapoint.previousDatapoint.level}
-                </span>{' '}
-              </>
+              <Badge
+                type="success"
+                title={
+                  datapoint.previousDatapoint &&
+                  datapoint.previousDatapoint.timestamp
+                    ? `Compared to: ${new Date(
+                        datapoint.previousDatapoint.timestamp
+                      ).toLocaleString()}`
+                    : undefined
+                }
+                label={`+${
+                  datapoint.datapoint.level - datapoint.previousDatapoint.level
+                }`}
+              />
             )}
-          <span
+          <Typography
             title={`XP: ${datapoint.datapoint.experience.toLocaleString()}`}
           >
             {datapoint.datapoint.level}
-          </span>
-        </td>
-      </tr>
+          </Typography>
+        </TableCell>
+      </TableRow>
       {(isSelected && (
-        <tr>
-          <td colSpan={4} className="bg-light">
+        <TableRow>
+          <TableCell colSpan={999}>
             <CharacterGraphContainer />
-          </td>
-        </tr>
+          </TableCell>
+        </TableRow>
       )) ||
         null}
     </>
