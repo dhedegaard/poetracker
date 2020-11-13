3b406391e3a18dc60b8104890219ffcf3131d8eb
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-11-13 17:46:30 +0100

    Simplified the program.

--- backend/Web/Program.cs
+++ backend/Web/Program.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Web {
-  public class Program {
-    public static void Main(string[] args) =>
-        BuildWebHost(args).Run();
+BuildWebHost(args).Run();
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
-    public static IWebHost BuildWebHost(string[] args) =>
-        CreateWebHostBuilder(args).Build();
-  }
-}
+static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Web.Startup>();
+static IWebHost BuildWebHost(string[] args) =>
+    CreateWebHostBuilder(args).Build();
